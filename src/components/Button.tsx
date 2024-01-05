31eb7b7d3fef5b7c568ed101214287099f83ffcf
jiokeokwuosa <softwareking@Chijiokes-MacBook-Air.local>
2024-01-05 12:54:37 +0100

    feature: stripe connect onboarding

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { Url } from 'url';
 
 interface IButton {
   label: JSX.Element | string;
@@ -16,9 +15,9 @@ const Button = ({ label, to = '', onClick, icon, className }: IButton) => {
   const ButtonComponent = to ? Link : 'button';
   return (
     <ButtonComponent
-      href={to}
+      href={to? to : 'empty'}
       onClick={onClick}
-      className={`font-gosha flex gap-[10px] justify-center items-center bg-primary text-black px-[18px] py-[25px] font-bold text-2xl leading-[30px] rounded-lg cursor-pointer transform hover:translate-y-[-1px] transition duration-300 hover:bg-primary-dark ${className} `}
+      className={`font-gosha flex gap-[10px] justify-center items-center bg-primary text-black px-[18px] py-[25px] font-bold text-2xl leading-[30px] rounded-lg cursor-pointer transform hover:translate-y-[-1px] transition duration-300 hover:bg-primary-dark ${className}`}
     >
       {icon ? icon : null}
       {label}
