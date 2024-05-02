265a4b55fe95bac689ce758aa2a7433b3b11e1ac
dev-newton <newtonimonjirie@gmail.com>
2024-05-02 10:17:06 +0100

    fix - PR feedback

--- src/app/auth/stripe/onboard-user/success/usePage.ts
+++ src/app/auth/stripe/onboard-user/success/usePage.ts
@@ -15,36 +15,6 @@ const usePage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { push } = useRouter();
 
-  const handleError = (result: { error: object }) => {
-    if (result.error) {
-      throw new Error(JSON.stringify(result));
-    }
-  };
-
-  const updateProducerRecordHandler = async (
-    result: object,
-    supplierId: string,
-    accountId: string
-  ) => {
-    if (result) {
-      await handleProducerRecordUpdate(supplierId, accountId);
-    } else {
-      push("/auth/stripe/onboard-user/refresh");
-    }
-  };
-
-  const updatePartnerRecordHandler = async (
-    result: object,
-    supplierId: string,
-    accountId: string
-  ) => {
-    if (result) {
-      await handlePartnerRecordUpdate(supplierId, accountId);
-    } else {
-      push("/auth/stripe/onboard-user/refresh");
-    }
-  };
-
   const stripeOnboardingSuccess = async () => {
     try {
       setIsLoading(true);
@@ -54,14 +24,12 @@ const usePage = () => {
 
       if (accountId) {
         let result = await handleGetStripeConnectAccountInfo(accountId);
-        if (supplierId) {
-          // check that charges are enabled in the user's stripe account
-          updateProducerRecordHandler(result, supplierId, accountId);
-          handleError(result);
-        } else if (partnerId) {
-          // check that charges are enabled in the user's stripe account
-          updatePartnerRecordHandler(result, partnerId, accountId);
-          handleError(result);
+        if (result && result.data.charges_enabled == true) {
+          supplierId
+            ? await handleProducerRecordUpdate(supplierId, accountId)
+            : await handlePartnerRecordUpdate(partnerId!, accountId);
+        } else {
+          push("/auth/stripe/onboard-user/refresh");
         }
       }
       setIsLoading(false);
